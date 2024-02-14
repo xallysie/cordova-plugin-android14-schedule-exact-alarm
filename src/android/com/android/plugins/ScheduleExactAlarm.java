f78dac68052b074ac26addf17ed20c6ddd8fe362
xallysie <sx6989@princeton.edu>
2024-02-14 00:26:59 -0500

    change functionality to open settings instead

--- src/android/com/android/plugins/ScheduleExactAlarm.java
+++ src/android/com/android/plugins/ScheduleExactAlarm.java
@@ -1,15 +1,11 @@
 package com.android.plugins;
 
-import android.app.Activity;
-import android.app.AlarmManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
+import android.content.Intent;
+import android.content.Context;
+import android.net.Uri;
+import android.app.AlarmManager;
 import android.provider.Settings;
-import android.util.Log;
-import android.Manifest;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -18,92 +14,29 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by Sally Xie (@xallysie) on 2023/09/14.
+ * Created by Sally Xie (@xallysie) on 2024/02/14.
  */
 
-public class ScheduleExactAlarm extends CordovaPlugin {
-
-    private static String TAG = "ScheduleExactAlarm";
-    private static final String ACTION_REQUEST_PERMISSION = "requestPermission";
-    private static final String KEY_ERROR = "error";
-    private static final String KEY_MESSAGE = "message";
-    private static final String KEY_RESULT_PERMISSION = "hasPermission";
-    private static final String SCHEDULE_EXACT_ALARM = "Manifest.permission.SCHEDULE_EXACT_ALARM";
-
-    private CallbackContext permissionsCallback;
+ public class ScheduleExactAlarm extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-        if (ACTION_REQUEST_PERMISSION.equals(action)) {
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    try {
-                        requestPermissionAction(callbackContext, data);
-                    } catch (Exception e){
-                        e.printStackTrace();
-                        JSONObject returnObj = new JSONObject();
-                        addProperty(returnObj, KEY_ERROR, ACTION_REQUEST_PERMISSION);
-                        addProperty(returnObj, KEY_MESSAGE, "Request permission has been denied.");
-                        callbackContext.error(returnObj);
-                        permissionsCallback = null;
-                    }
-                }
-            });
+        if (action.equals("openAlarmsSetting")) {
+            this.openAlarmsSetting(callbackContext);
             return true;
-        } return false;
+        }
+        return false;
     }
 
-    private void requestPermissionAction(CallbackContext callbackContext, JSONArray permissions) throws Exception {
-        if (permissions == null || permissions.length() == 0) {
-            JSONObject returnObj = new JSONObject();
-            addProperty(returnObj, KEY_ERROR, ACTION_REQUEST_PERMISSION);
-            addProperty(returnObj, KEY_MESSAGE, "At least one permission.");
-            callbackContext.error(returnObj);
+    private void openAlarmsSetting(CallbackContext callbackContext) {
+        AlarmManager alarmManager = (AlarmManager) cordova.getActivity().getSystemService(Context.ALARM_SERVICE);
+        if (!alarmManager.canScheduleExactAlarms()) {
+            Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+            intent.setData(Uri.parse("package:" + cordova.getActivity().getPackageName()));
+            cordova.getActivity().startActivity(intent);
+            callbackContext.success();
         } else {
-            permissionsCallback = callbackContext;
-            String[] permissionArray = getPermissions(permissions);
-            if (permissionArray.length == 1 && SCHEDULE_EXACT_ALARM.equals(permissionArray[0])) {
-                AlarmManager alarmManager = (AlarmManager) cordova.getActivity().getSystemService(Context.ALARM_SERVICE);
-                if (!alarmManager.canScheduleExactAlarms()) {
-                    Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
-                    intent.setData(Uri.parse("package:" + cordova.getActivity().getPackageName()));
-                    cordova.getActivity().startActivity(intent);
-                    JSONObject returnObj = new JSONObject();
-                    addProperty(returnObj, KEY_RESULT_PERMISSION, false);
-                    callbackContext.success(returnObj);
-                } else {
-                    JSONObject returnObj = new JSONObject();
-                    addProperty(returnObj, KEY_RESULT_PERMISSION, true);
-                    callbackContext.success(returnObj);
-                }
-                return;
-            }
-            cordova.requestPermissions(this, 0, permissionArray);
-        }
-    }
-
-    private String[] getPermissions(JSONArray permissions) {
-        String[] stringArray = new String[permissions.length()];
-        for (int i = 0; i < permissions.length(); i++) {
-            try {
-                stringArray[i] = permissions.getString(i);
-            } catch (JSONException ignored) {
-                //Believe exception only occurs when adding duplicate keys, so just ignore it
-            }
-        }
-        return stringArray;
-    }
-
-    private void addProperty(JSONObject obj, String key, Object value) {
-        try {
-            if (value == null) {
-                obj.put(key, JSONObject.NULL);
-            } else {
-                obj.put(key, value);
-            }
-        } catch (JSONException ignored) {
-            //Believe exception only occurs when adding duplicate keys, so just ignore it
+            callbackContext.error("App already has permission to schedule exact alarms.");
         }
     }
 }
--- www/scheduleexactalarm.js
+++ www/scheduleexactalarm.js
@@ -1,19 +1,8 @@
 var exec = require('cordova/exec');
 
 var ScheduleExactAlarm = {
-    checkPermission: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'ScheduleExactAlarm', 'checkPermission', []);
-    },
-    requestPermission: function(successCallback, errorCallback) {
-        if (typeof successCallback === "function") {
-            var temp = successCallback;
-            successCallback = errorCallback;
-            errorCallback = temp;
-        }
-        exec(successCallback, errorCallback, 'ScheduleExactAlarm', 'requestPermission', []);
-    },
-    requestPermissions: function(permissions, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'ScheduleExactAlarm', 'requestPermissions', [permissions]);
+    openAlarmsSetting: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'ScheduleExactAlarm', 'openAlarmsSetting', []);
     }
 };
 
